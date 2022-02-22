3667c30c910beec57cdb62ebe80502e769d61162
lortegaperal <lortegaperal@safareyes.es>
2022-02-22 19:44:56 +0100

    primer metodo

--- src/main/java/modelos/Escalabilidad.java
+++ src/main/java/modelos/Escalabilidad.java
@@ -1,9 +1,84 @@
 package modelos;
 
+import java.util.Objects;
+
 public class Escalabilidad {
+
     private int id;
     private Double incremetoDanoNivel;
     private Double incremetoDefensaNivel;
     private Double incremetoSaludNivel;
     private Double incremetoManaNivel;
+
+    public Escalabilidad(int id, Double incremetoDanoNivel, Double incremetoDefensaNivel, Double incremetoSaludNivel, Double incremetoManaNivel) {
+        this.id = id;
+        this.incremetoDanoNivel = incremetoDanoNivel;
+        this.incremetoDefensaNivel = incremetoDefensaNivel;
+        this.incremetoSaludNivel = incremetoSaludNivel;
+        this.incremetoManaNivel = incremetoManaNivel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getIncremetoDanoNivel() {
+        return incremetoDanoNivel;
+    }
+
+    public void setIncremetoDanoNivel(Double incremetoDanoNivel) {
+        this.incremetoDanoNivel = incremetoDanoNivel;
+    }
+
+    public Double getIncremetoDefensaNivel() {
+        return incremetoDefensaNivel;
+    }
+
+    public void setIncremetoDefensaNivel(Double incremetoDefensaNivel) {
+        this.incremetoDefensaNivel = incremetoDefensaNivel;
+    }
+
+    public Double getIncremetoSaludNivel() {
+        return incremetoSaludNivel;
+    }
+
+    public void setIncremetoSaludNivel(Double incremetoSaludNivel) {
+        this.incremetoSaludNivel = incremetoSaludNivel;
+    }
+
+    public Double getIncremetoManaNivel() {
+        return incremetoManaNivel;
+    }
+
+    public void setIncremetoManaNivel(Double incremetoManaNivel) {
+        this.incremetoManaNivel = incremetoManaNivel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Escalabilidad that = (Escalabilidad) o;
+        return id == that.id && Objects.equals(incremetoDanoNivel, that.incremetoDanoNivel) && Objects.equals(incremetoDefensaNivel, that.incremetoDefensaNivel) && Objects.equals(incremetoSaludNivel, that.incremetoSaludNivel) && Objects.equals(incremetoManaNivel, that.incremetoManaNivel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, incremetoDanoNivel, incremetoDefensaNivel, incremetoSaludNivel, incremetoManaNivel);
+    }
+
+    @Override
+    public String toString() {
+        return "Escalabilidad{" +
+                "id=" + id +
+                ", incremetoDanoNivel=" + incremetoDanoNivel +
+                ", incremetoDefensaNivel=" + incremetoDefensaNivel +
+                ", incremetoSaludNivel=" + incremetoSaludNivel +
+                ", incremetoManaNivel=" + incremetoManaNivel +
+                '}';
+    }
 }
--- src/main/java/utilidades/UtilidadesPersonaje.java
+++ src/main/java/utilidades/UtilidadesPersonaje.java
@@ -1,4 +1,22 @@
 package utilidades;
 
+import modelos.Escalabilidad;
+import modelos.Personaje;
+
+
 public class UtilidadesPersonaje {
+
+    public Personaje levelUp(Personaje personaje){
+
+       Integer subida_nivel = personaje.getNivel() + 1;
+
+       Double ataque = personaje.getAtaqueBase() + (subida_nivel * personaje.getEscalabilidad().getIncremetoDanoNivel());
+       Double defensa = personaje.getDefensaBase() + (subida_nivel * personaje.getEscalabilidad().getIncremetoDefensaNivel());
+       Double vida = personaje.getVidaBase() + (subida_nivel * personaje.getEscalabilidad().getIncremetoSaludNivel());
+       Double mana = personaje.getMana() + (subida_nivel * personaje.getEscalabilidad().getIncremetoManaNivel());
+
+        return personaje;
+    }
+
+
 }
